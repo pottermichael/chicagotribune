14d835b86c141b41e47b32653ec02aa3a9a78c4f
pottermichael <potter.michael@gmail.com>
2020-12-17 13:54:12 -0600

    add dotenv package and .flaskenv file

--- project/models.py
+++ project/models.py
@@ -6,3 +6,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100),unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
